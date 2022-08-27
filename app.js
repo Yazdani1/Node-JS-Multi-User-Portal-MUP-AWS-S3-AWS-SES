31fc804d6ea6737793c245af3f59e8bfc9ea3338
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-08-27 11:39:45 +0300

    Node JS Installed

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("dgdffd")
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require('path');
+
+require("./model/db");
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// app.use("/api",require("./router/user"));
+
+
+app.listen(port, (req, res) => {
+  console.log("Server connected");
+});
